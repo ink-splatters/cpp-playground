3233bbe403d088d6c888364822fbe7d4c59fc603
Peter A. <ink.splatters@pm.me>
2024-02-01 14:06:50 +0100

    locked flake

--- flake.nix
+++ flake.nix
@@ -5,19 +5,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
         # pkgs = import nixpkgs {
         #   inherit system;
         #   config.allowUnsupportedSystem = true;
         # };
 
         CXXFLAGS = "-std=c++20 -stdlib=libc++";
-        LDFLAGS= "-lc++";
+        LDFLAGS = "-lc++";
         name = "cpp-playground";
 
-        nativeBuildInputs = with llvmPackages_latest; [
-            ninja
-            meson
-            ( with llvmPackages_latest; [ llvm lldb clang-tools])
+        nativeBuildInputs = with pkgs;[
+          ninja
+          meson
+          (with llvmPackages_latest; [ llvm lldb clang-tools ])
         ];
 
         # buildInputs = [
@@ -25,16 +26,18 @@
         #   ...
         # ]
 
-      in with pkgs ; {
+      in
+      with pkgs ; {
         formatter = nixpkgs-fmt;
 
-        devShells.default = mkShell.override  { inherit llvmPackages_latest.stdenv; } {
+        devShells.default = mkShell.override { inherit (llvmPackages_latest) stdenv; } {
           inherit nativeBuildInputs CXXFLAGS; # LDFLAGS;
 
-        shellHook = ''
-                export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
-        '';
+          shellHook = ''
+            export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+          '';
         };
+
 
         # packages.default = mkDerivation {
         #   inherit name;
@@ -46,4 +49,5 @@
 
         #   ''
         # }
+      });
 }
